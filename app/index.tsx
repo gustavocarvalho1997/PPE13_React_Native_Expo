60fe1d441829f74cf6c2d7ba9bec4b9a398c8ac6
gustavocarvalho1997 <gustavocarvalho1997@hotmail.com>
2025-04-25 18:23:26 -0300

    feat: add initial layout and button components with basic navigation

---
+++ app/index.tsx
@@ -0,0 +1,44 @@
+import { Image, SafeAreaView, StyleSheet, Text } from "react-native";
+import { Button } from "../components/button";
+import { router } from "expo-router";
+
+export default function Screen() {
+    const handleStart = () => {
+        router.replace("/home");
+    };
+    return (
+        <SafeAreaView style={styles.container}>
+            <Image
+                source={require("../assets/logo.png")}
+                style={styles.logo}
+                resizeMode="cover"
+            />
+            <Text style={styles.h1}>Loja da Clarice</Text>
+            <Text style={styles.h2}>Aqui você encontra o que precisa!</Text>
+            <Button title="Começar as compras" onPress={handleStart} />
+        </SafeAreaView>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    logo: {
+        width: 200,
+        height: 200,
+        marginBottom: 20,
+    },
+    h1: {
+        fontSize: 22,
+        fontWeight: "bold",
+        marginBottom: 10,
+    },
+    h2: {
+        fontSize: 16,
+        color: "#555",
+        marginBottom: 10,
+    },
+});
