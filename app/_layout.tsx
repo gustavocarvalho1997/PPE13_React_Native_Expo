1a6cbde995c5f5dd220e4bc111dc7dfe7ffaa2b3
gustavocarvalho1997 <gustavocarvalho1997@hotmail.com>
2025-04-26 16:26:39 -0300

    feat: add TabLayout component with tab navigation for home, categories, and about

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,6 +4,7 @@ export default function RootLayout() {
     return (
         <Stack initialRouteName="index" screenOptions={{ headerShown: false }}>
             <Stack.Screen name="index" />
+            <Stack.Screen name="(tabs)" />
         </Stack>
     );
 }
